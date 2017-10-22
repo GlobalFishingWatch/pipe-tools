1e857be70a9be08a02610bffc7e5e84e1d2d2517
Paul Woods <pwoods25443@gmail.com>
2017-10-22 08:29:57 -0400

    initial release

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
-PROJECT_NAME = 'pipe-tools'
-PROJECT_VERSION = '1.0.0'
-PROJECT_DESCRIPTION = 'Apache Beam pipeline tools'
+"""
+Setup script for pipe-tools
+"""
+
+import codecs
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
 
 DEPENDENCIES = [
     "pytest",
@@ -57,13 +64,43 @@ DATAFLOW_PINNED_DEPENDENCIES = [
     "urllib3==1.22",
 ]
 
+with codecs.open('README.md', encoding='utf-8') as f:
+    readme = f.read().strip()
+
+
+version = None
+author = None
+email = None
+source = None
+with open(os.path.join('pipe_tools', '__init__.py')) as f:
+    for line in f:
+        if line.strip().startswith('__version__'):
+            version = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__author__'):
+            author = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__email__'):
+            email = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__source__'):
+            source = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif None not in (version, author, email, source):
+            break
+
+
 setup(
-    name=PROJECT_NAME,
-    version=PROJECT_VERSION,
-    description=PROJECT_DESCRIPTION,
-    author="Global Fishing Watch",
-    author_email="info@globalfishingwatch.org",
-    license="Apache 2",
-    packages=find_packages(),
+    author=author,
+    author_email=email,
+    description="A python utility library for apache beam and bigquery",
+    include_package_data=True,
     install_requires=DEPENDENCIES + DATAFLOW_PINNED_DEPENDENCIES,
+    keywords='AIS GIS remote sensing',
+    license="Apache 2.0",
+    long_description=readme,
+    name='pipe-tools',
+    packages=find_packages(exclude=['test*.*', 'tests']),
+    url=source,
+    version=version,
+    zip_safe=True
 )
+
+
+
