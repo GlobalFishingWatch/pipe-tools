2a91b6e1652a3929243ca0698cbf0e8d242da26f
Paul Woods <pwoods25443@gmail.com>
2017-11-23 14:51:41 -0500

    unit test now fails on #10

--- tests/test_datepartitionedsink.py
+++ tests/test_datepartitionedsink.py
@@ -5,6 +5,7 @@ from apache_beam.testing.test_pipeline import TestPipeline as _TestPipeline
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 from apache_beam.transforms.window import TimestampedValue
+from apache_beam.io import FileBasedSink
 
 from pipe_tools.io.datepartitionedsink import DatePartitionedFileSink
 from pipe_tools.io.datepartitionedsink import WriteToDatePartitionedFiles
@@ -27,7 +28,8 @@ class TestDatePartitionedSink():
             yield JSONDict(mmsi=1, timestamp= ts)
             ts += increment
 
-    def test_as_pipeline(self, temp_dir):
+    @pytest.mark.parametrize("shards_per_day", [1,2])
+    def test_as_pipeline(self, temp_dir, shards_per_day):
 
         file_path_base = temp_dir
         # file_path_base = 'gs://paul-scratch/TestDatePartitionedSink_temp'
@@ -40,14 +42,23 @@ class TestDatePartitionedSink():
                  for msg in messages}
 
         with _TestPipeline() as p:
+            writer = WriteToDatePartitionedFiles(file_path_prefix, file_name_suffix,
+                                                            shards_per_day=shards_per_day)
             messages = (
                 p
                 | beam.Create(messages)
                 | beam.Map(lambda msg: (TimestampedValue(msg, msg['timestamp'])))
             )
 
-            result = messages | WriteToDatePartitionedFiles(file_path_prefix, file_name_suffix, shards_per_day=1)
-            expected = ['%s%s%s'% (pp.join(file_path_base, date, file_name_prefix),
-                                   '-00000-of-00001', file_name_suffix)for date in dates]
+            result = messages | writer
+
+            expected = []
+            for date in dates:
+                for shard_num in range(shards_per_day):
+                    expected.append (
+                        ''.join([pp.join(file_path_base, date, file_name_prefix), writer._sink.shard_name_format % dict(
+                            shard_num=shard_num, num_shards=shards_per_day), file_name_suffix]
+                    ))
+
             assert_that(result, equal_to(expected))
 
