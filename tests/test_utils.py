05d3584a802cb7282bc94578b59488668df1f252
Paul Woods <pwoods25443@gmail.com>
2018-02-28 22:18:38 -0500

    support date in as_timestamp

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 from datetime import datetime
+from datetime import date
 
 from pipe_tools.utils import flatten
 from pipe_tools.utils import GFW_UUID
@@ -23,7 +24,8 @@ class Test_Utils:
         (None,None),
         ("2017-07-20T05:59:35.000000Z", 1500530375.0),
         (datetime(2017,7,20,5,59,35), 1500530375.0),
-        (1500530375.0, 1500530375.0)
+        (1500530375.0, 1500530375.0),
+        (date(2018,1,1), 1514764800.0)
     ])
     def test_as_timestamp(self, value, expected):
         assert as_timestamp(value) == expected
