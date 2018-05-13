29d5d4a6151accd2427abfdcc18eaa7f3c080d91
Paul Woods <pwoods25443@gmail.com>
2018-05-13 14:11:25 -0400

    disable exponetial backoff for source sensors

--- pipe_tools/airflow/models.py
+++ pipe_tools/airflow/models.py
@@ -74,7 +74,8 @@ class DagFactory(object):
             poke_interval=10,  # check every 10 seconds for a minute
             timeout=60,
             retries=24 * 7,  # retry once per hour for a week
-            retry_delay=timedelta(minutes=60)
+            retry_delay=timedelta(minutes=60),
+            retry_exponential_backoff=False
         )
 
     def source_table_sensors(self, dag):
