745a8b84437eb2f6849a39e8b68da266b3bde554
bitsofbits <tim@bitsofbits.com>
2018-10-10 16:54:55 -0700

    implement Enrique's suggested changes

--- pipe_tools/airflow/__init__.py
+++ pipe_tools/airflow/__init__.py
@@ -1,3 +1,6 @@
 # Monkey patch TaskInstance to get tests to run -- probably a better solution
 from airflow.models import TaskInstance
+# TaskInstance.retries Tests began failing because an underlying package expects TaskInstances to have a
+# `max_retries` parameter. I'd guess some sort of versioning issue. We set `TaskInstance.max_tries` to
+# 16 to make the underlying package happy. I'm not certain what it's used for and the 16 is arbitrary.
 TaskInstance.max_tries = 16
