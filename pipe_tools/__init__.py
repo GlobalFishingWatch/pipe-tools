6efaab010a595b3bf472c7486d36ecfde9d298f5
Enrique Tuya <enriquetuya@gmail.com>
2018-11-17 01:38:51 -0300

    Change the version of the package.

--- pipe_tools/__init__.py
+++ pipe_tools/__init__.py
@@ -3,7 +3,7 @@ Tools for running the GFW pipeline.
 """
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.4'
 __author__ = 'Paul Woods'
 __email__ = 'paul@globalfishingwatch.org'
 __source__ = 'https://github.com/GlobalFishingWatch/pipe-tools'
